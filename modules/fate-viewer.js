c0bc176607522c78d6823a48a410c38b5943c571
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-08-23 03:45:05 -0300

    Fate viewer fix

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -7,6 +7,6 @@ module.exports = async (fatedata) => {
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
   ctx.font = '27px dejavusans';
   ctx.fillStyle = '#fffff2';
-  ctx.fillText(fatedata.name, 88, 150);
+  ctx.fillText(fatedata.name, 88, 140);
   return canvas.toBuffer();
 };
