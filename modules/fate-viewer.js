f1cae10470d30147b7c804ef72e14f15114efce8
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-08-23 03:07:52 -0300

    Fate viewer test fix

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -4,7 +4,7 @@ registerFont(__dirname+"/../bin/DejaVuSans.ttf", { family: 'dejavusans'});
 module.exports = (fatedata) => {
   const canvas = createCanvas(1080, 1620);
   const ctx = canvas.getContext('2d');
-  ctx.drawImage(await loadImage(__dirname+'/../bin/ftbackground.png'), 0, 0);
+  ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
   ctx.font = '12px dejavusans';
   ctx.fillText(fatedata.name, 10, 10);
   return canvas.toBuffer();
