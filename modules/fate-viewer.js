78340d8050786cba57c09b2b9d25b8efc1d0c65e
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-08-23 03:14:45 -0300

    Async ftviewer

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -1,7 +1,7 @@
 const { createCanvas, loadImage, registerFont } = require('canvas');
 
 registerFont(__dirname+"/../bin/DejaVuSans.ttf", { family: 'dejavusans'});
-module.exports = (fatedata) => {
+module.exports = async (fatedata) => {
   const canvas = createCanvas(1080, 1620);
   const ctx = canvas.getContext('2d');
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
