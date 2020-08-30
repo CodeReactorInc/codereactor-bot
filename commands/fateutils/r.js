a070d2fa0e24e3270b1616719ff336fd72efa123
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-08-30 00:55:52 -0300

    Usage fix

--- commands/fateutils/r.js
+++ commands/fateutils/r.js
@@ -2,5 +2,5 @@ const alias = require('./roll.js');
 exports.run = alias.run;
 exports.help = alias.help;
 
-exports.help.usage = ["r[dice]"];
+exports.help.usage = ["r [dice]"];
 exports.help.name = "r";
