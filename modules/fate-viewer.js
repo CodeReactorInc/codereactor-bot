09ad4e660eb9cdb02aee4c2139908240909214ac
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-09-11 08:14:50 -0300

    Fate name fix

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -5,8 +5,8 @@ module.exports = async (fatedata) => {
   const canvas = createCanvas(1080, 1620);
   const ctx = canvas.getContext('2d');
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
-  ctx.font = '26px dejavusans';
+  ctx.font = '25px dejavusans';
   ctx.fillStyle = '#fffff2';
-  ctx.fillText(fatedata.name, 86, 138);
+  ctx.fillText(fatedata.name, 84, 136);
   return canvas.toBuffer();
 };
