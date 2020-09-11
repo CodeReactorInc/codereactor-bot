71678626bc357ed96dd0ce549ed65b63b5c62e24
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-09-11 08:48:37 -0300

    Fate viewer fix

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -1,12 +1,23 @@
 const { createCanvas, loadImage, registerFont } = require('canvas');
 
+const { getLogger } = require('./libs/logger.js');
+const fs = require('fs');
+const streams = [ fs.createWriteStream(__dirname+'/../logs/latest.log', { flags: 'a' }), process.stdout ];
+
+const logger = getLogger("Fate Viewer", streams);
+
 registerFont(__dirname+"/../bin/DejaVuSans.ttf", { family: 'dejavusans'});
 module.exports = async (fatedata) => {
+  logger.info("Creating canvas with 1080x1620...");
   const canvas = createCanvas(1080, 1620);
+  logger.info("Getting a 2D context from canvas...");
   const ctx = canvas.getContext('2d');
+  logger.info("Drawing image...");
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
+  logger.info("Setting style...");
   ctx.font = '26px dejavusans';
   ctx.fillStyle = '#fffff2';
+  logger.info("Setting 'name' in "+84+"x"+136);
   ctx.fillText(fatedata.name, 84, 136);
   let descLines = [];
   let descLine = "";
@@ -27,6 +38,7 @@ module.exports = async (fatedata) => {
   let lastDescValue = 158;
   for(let i = 0;i < descLines.length;i++) {
     if (ctx.measureText(descLines[i]).height < 136) {
+      logger.info("Setting 'description' in "+84+"x"+lastDescValue);
       ctx.fillText(descLines[i], 84, lastDescValue);
       lastDescValue += ctx.measureText(descLines[i]).height;
     }
