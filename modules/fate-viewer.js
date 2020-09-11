1aaf2ae5a0cffb725e213d461825a1ff97d53725
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-09-11 08:10:11 -0300

    New name size

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -5,8 +5,9 @@ module.exports = async (fatedata) => {
   const canvas = createCanvas(1080, 1620);
   const ctx = canvas.getContext('2d');
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
-  ctx.font = '27px dejavusans';
+  ctx.font = '26px dejavusans';
   ctx.fillStyle = '#fffff2';
-  ctx.fillText(fatedata.name, 88, 138);
+  ctx.fillText(fatedata.name, 86, 138);
+  ctx.fillStyle(fatedata)
   return canvas.toBuffer();
 };
