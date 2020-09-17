f8b4f799abaffc510dbfdf0e2388e2b6420e4b84
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-09-17 11:49:29 -0300

    Quick fix

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -50,7 +50,7 @@ module.exports = async (fatedata) => {
   logger.info("Setting 'destiny_points' in "+dpwidth+"x"+265);
   ctx.fillText(fatedata.destiny_points, dpwidth, 265);
   ctx.font = '38px dejavusans-bold';
-  logger.info("Setting 'agile' in "+930+"x"+411);
-  ctx.fillText("+"+fatedata.agile, 930, 411);
+  logger.info("Setting 'agile' in "+925+"x"+413);
+  ctx.fillText("+"+fatedata.agile, 925, 413);
   return canvas.toBuffer();
 };
