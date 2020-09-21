e8d7d5de24510ab4b897ee1a60324430507b796a
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-09-21 08:35:35 -0300

    Approach fix and added aspect concept

--- modules/fate-viewer.js
+++ modules/fate-viewer.js
@@ -13,13 +13,16 @@ module.exports = async (fatedata) => {
   const canvas = createCanvas(1080, 1620);
   logger.info("Getting a 2D context from canvas...");
   const ctx = canvas.getContext('2d');
+
   logger.info("Drawing image...");
   ctx.drawImage((await loadImage(__dirname+'/../bin/ftbackground.png')), 0, 0);
+
   logger.info("Setting style...");
   ctx.font = '26px dejavusans';
   ctx.fillStyle = '#fffff2';
   logger.info("Setting 'name' in "+84+"x"+136);
   ctx.fillText(fatedata.name, 84, 136);
+
   let descLines = [];
   let descLine = "";
   for(let i = 0;i < fatedata.description.length;i++) {
@@ -34,33 +37,42 @@ module.exports = async (fatedata) => {
       descLine += char;
     }
   }
+
   descLines.push(descLine);
   descLine = "";
   let lastDescValue = 190;
+
   logger.info("Description has "+descLines.length+" lines");
   for(let i = 0;i < descLines.length && i < 5;i++) {
     logger.info("Setting 'description' in "+84+"x"+lastDescValue);
     ctx.fillText(descLines[i], 84, lastDescValue);
     lastDescValue += 25;
   }
+
   logger.info("Setting 'recharge' in "+740+"x"+265);
   ctx.font = '60px dejavusans-bold';
   ctx.fillText("+"+fatedata.recharge, 740, 265);
+
   let dpwidth = (943 + 21) - ((ctx.measureText(fatedata.destiny_points).width / 42)  * 21);
   logger.info("Setting 'destiny_points' in "+dpwidth+"x"+265);
   ctx.fillText(fatedata.destiny_points, dpwidth, 265);
+
   ctx.font = '38px dejavusans-bold';
   logger.info("Setting 'agile' in "+915+"x"+412);
   ctx.fillText("+"+fatedata.agile, 915, 412);
   logger.info("Setting 'careful' in "+915+"x"+469);
   ctx.fillText("+"+fatedata.careful, 915, 469);
-  logger.info("Setting 'smart' in "+915+"x"+526);
-  ctx.fillText("+"+fatedata.smart, 915, 526);
+  logger.info("Setting 'smart' in "+915+"x"+525);
+  ctx.fillText("+"+fatedata.smart, 915, 525);
   logger.info("Setting 'stylish' in "+915+"x"+581);
   ctx.fillText("+"+fatedata.stylish, 915, 581);
   logger.info("Setting 'power' in "+915+"x"+637);
   ctx.fillText("+"+fatedata.power, 915, 637);
   logger.info("Setting 'sneaky' in "+915+"x"+694);
   ctx.fillText("+"+fatedata.sneaky, 915, 694);
+
+  ctx.font = '26px dejavusans';
+  logger.info("Setting 'aspect_concept' in "+84+"x"+412);
+  ctx.fillText(fatedata.aspect_concept, 84, 412);
   return canvas.toBuffer();
 };
