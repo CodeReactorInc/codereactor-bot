3e3e99b516dd85ed6cfe2541c61d072290c0a0f4
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-10-06 08:30:31 -0300

    Bot logger created

---
+++ commands/general/bot-logger.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+exports.run = async (client, message, args, data) => {
+  data.logger.info("Testing if message author are the bot owner...");
+
+  if (message.author.id !== data.config.OWNER_ID) {
+    data.logger.warn("Message author aren't the owner: "+message.author.id);
+    message.channel.send("You aren't the bot owner");
+    return;
+  }
+
+  data.logger.info("Reading latest.log...");
+  let log = fs.readFileSync(__dirname + '/../../logs/latest.log');
+  data.logger.info("Building lines list and inverting...");
+  let lines = log.split('\n').reverse();
+  var msg = "";
+  data.logger.info("Creating msg limited in 2000 chars...");
+  for(var i = 0;i < lines.length;i++) {
+    if ((msg + lines[i] + "\n").length > 2000) break;
+    msg += lines[i] + "\n";
+  }
+  data.logger.info("Sending logger...");
+  message.channel.send(msg);
+};
+
+exports.help = {
+  description: "Read and send the logger data",
+  usage: ["bot-logger"],
+  name: "bot-logger",
+  permissions: "bot owner"
+};
