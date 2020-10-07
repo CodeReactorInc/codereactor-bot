b51ab100b2e7b4a4f2018ce4a6ade00dd3462bab
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-10-07 10:05:03 -0300

    Invite command

---
+++ commands/general/invite.js
@@ -0,0 +1,12 @@
+exports.run = async (client, message, args, data) => {
+  data.logger.info("Generating Discord bot invite...");
+  let link = await client.generateInvite(["KICK_MEMBERS", "BAN_MEMBERS", "ADD_REACTIONS", "VIEW_CHANNEL", "VIEW_CHANNEL", "SEND_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "MANAGE_WEBHOOKS"]);
+  message.channel.send("**Bot invite:** "+link+"\n**Server invite:** https://discord.gg/Jja9n2H");
+};
+
+exports.help = {
+  description: "Get help about a module or command",
+  usage: ["help", "help [module]", "help [command]"],
+  name: "help",
+  permissions: "everyone"
+};
