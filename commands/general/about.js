347103e46a76c380f4355227e8dafdfc557f3f19
KitsuneIDK <50382775+KitsuneIDK@users.noreply.github.com>
2020-10-07 09:14:35 -0300

    Added about command

---
+++ commands/general/about.js
@@ -0,0 +1,32 @@
+exports.run = async (client, message, args, data) => {
+  data.logger.info("Starting to create the message...");
+  var msg = "**Dev info:**\n"+
+    "Developer: Code Reactor\n"+
+    "Runtime: NodeJS\n"+
+    "Lang: JavaScript\n"+
+    "Library: DiscordJS\n\n"
+    "Bot version: "+require('../../package.json').version+"\n";
+    "Update branch: master\n\n";
+
+  data.logger.info("Collecting info from client...");
+  msg += "**Client info:**\nClient ping: " + client.ws.ping + "ms\n";
+  let date = new Date(client.uptime);
+  msg += "Client uptime: "+date.getDay()+" days, "+date.getHours()+" hours, "+date.getMinutes()+" minutes, "+date.getSeconds()+" seconds\n";
+  msg += "Client shards: "+client.ws.shards.size+" shards\n";
+  msg += "Client version: "+require('../../package-lock.json').dependencies["discord.js"].version+"\n";
+
+  data.logger.info("Collection info from system...");
+  msg += "**System info:**\n"+
+    "NodeJS Version: "+process.version+"\n"+
+    "Platform: "+process.platform+"\n"+
+    "Arch: "+process.arch+"\n"+
+    "CPU Usage: "+process.cpuUsage().user+"/"+process.cpuUsage().system+"\n"+
+    "Memory Usage: "+process.memoryUsage().heapUsed+"/"+process.memoryUsage().heapTotal+"\n";
+};
+
+exports.help = {
+  description: "Ban the mentioned user from server",
+  usage: ["ban [id]", "ban [mention]", "ban [id] [message]", "ban [mention] [message]"],
+  name: "ban",
+  permissions: "ban members"
+};
